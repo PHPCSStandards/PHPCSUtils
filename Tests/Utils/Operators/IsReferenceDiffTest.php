8e2079ed90b1e4f40a7af99612ccec4e397ef317
jrfnl <jrfnl@users.noreply.github.com>
2020-01-22 19:48:44 +0100

    Operators::isReference(): add tests for select tokenizer issues

--- Tests/Utils/Operators/IsReferenceDiffTest.php
+++ Tests/Utils/Operators/IsReferenceDiffTest.php
@@ -37,4 +37,54 @@ class IsReferenceDiffDiffTest extends UtilityMethodTestCase
     {
         $this->assertFalse(Operators::isReference(self::$phpcsFile, 10000));
     }
+
+    /**
+     * Test correctly identifying that whether a "bitwise and" token is a reference or not.
+     *
+     * @dataProvider dataIsReference
+     *
+     * @param string $identifier Comment which precedes the test case.
+     * @param bool   $expected   Expected function output.
+     *
+     * @return void
+     */
+    public function testIsReference($identifier, $expected)
+    {
+        $bitwiseAnd = $this->getTargetToken($identifier, \T_BITWISE_AND);
+        $result     = Operators::isReference(self::$phpcsFile, $bitwiseAnd);
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testIsReference()
+     *
+     * @return array
+     */
+    public function dataIsReference()
+    {
+        return [
+            'issue-1971-list-first-in-file' => [
+                '/* testTokenizerIssue1971PHPCSlt330gt271A */',
+                true,
+            ],
+            'issue-1971-list-first-in-file-nested' => [
+                '/* testTokenizerIssue1971PHPCSlt330gt271B */',
+                true,
+            ],
+            'issue-1284-short-list-directly-after-close-curly-control-structure' => [
+                '/* testTokenizerIssue1284PHPCSlt280A */',
+                true,
+            ],
+            'issue-1284-short-list-directly-after-close-curly-control-structure-second-item' => [
+                '/* testTokenizerIssue1284PHPCSlt280B */',
+                true,
+            ],
+            'issue-1284-short-array-directly-after-close-curly-control-structure' => [
+                '/* testTokenizerIssue1284PHPCSlt280C */',
+                true,
+            ],
+        ];
+    }
 }
