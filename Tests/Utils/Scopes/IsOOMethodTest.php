e99f0bcf9e6e6403000e20152d0c0314545931c7
jrfnl <jrfnl@users.noreply.github.com>
2020-01-22 02:09:08 +0100

    :sparkles: New Utils\Scopes class
    New `PHPCSUtils\Utils\Scopes` class which will hold methods related to the scoping of tokens.
    This class is closely related to the `PHPCSUtils\Utils\Conditions` class in that it uses the `$tokens[$stackPtr]['conditions']` index.
    
    This class initially contains the following four utility methods:
    * `isOOProperty()` - to check if a `T_VARIABLE` token is a class/trait property. Returns true/false.
    * `isOOConstant()` - to check if a `T_CONSTANT` token is a class/interface constant.  Returns true/false.
    * `isOOMethod()` - to check if a `T_FUNCTION` token is a class/interface/trait method.  Returns true/false.
    * `validDirectScope()` - to check the direct condition of an arbitrary token against a set of valid scope conditions. Returns the integer stackPtr to the direct condition if valid or false otherwise.
    
    These methods were written by me and have been battle-tested in the past year or two in the PHPCompatibility standard.
    
    Includes dedicated unit tests.

---
+++ Tests/Utils/Scopes/IsOOMethodTest.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * PHPCSUtils, utility functions and classes for PHP_CodeSniffer sniff developers.
+ *
+ * @package   PHPCSUtils
+ * @copyright 2019 PHPCSUtils Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSUtils
+ */
+
+namespace PHPCSUtils\Tests\Utils\Scopes;
+
+use PHPCSUtils\TestUtils\UtilityMethodTestCase;
+use PHPCSUtils\Utils\Scopes;
+
+/**
+ * Tests for the \PHPCSUtils\Utils\Scopes::isOOMethod() method.
+ *
+ * @group scopes
+ *
+ * @since 1.0.0
+ */
+class IsOOMethodTest extends UtilityMethodTestCase
+{
+
+    /**
+     * Test passing a non-existent token pointer.
+     *
+     * @covers \PHPCSUtils\Utils\Scopes::isOOMethod
+     *
+     * @return void
+     */
+    public function testNonExistentToken()
+    {
+        $result = Scopes::isOOMethod(self::$phpcsFile, 10000);
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Test passing a non function token.
+     *
+     * @covers \PHPCSUtils\Utils\Scopes::isOOMethod
+     *
+     * @return void
+     */
+    public function testNonFunctionToken()
+    {
+        $result = Scopes::isOOMethod(self::$phpcsFile, 0);
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Test correctly identifying whether a T_FUNCTION token is a class method declaration.
+     *
+     * @dataProvider dataIsOOMethod
+     *
+     * @covers \PHPCSUtils\Utils\Scopes::isOOMethod
+     * @covers \PHPCSUtils\Utils\Scopes::validDirectScope
+     *
+     * @param string $testMarker The comment which prefaces the target token in the test file.
+     * @param bool   $expected   The expected function return value.
+     *
+     * @return void
+     */
+    public function testIsOOMethod($testMarker, $expected)
+    {
+        $stackPtr = $this->getTargetToken($testMarker, [\T_FUNCTION, \T_CLOSURE]);
+        $result   = Scopes::isOOMethod(self::$phpcsFile, $stackPtr);
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testIsOOMethod() For the array format.
+     *
+     * @return array
+     */
+    public function dataIsOOMethod()
+    {
+        return [
+            'global-function' => [
+                '/* testGlobalFunction */',
+                false,
+            ],
+            'nested-function' => [
+                '/* testNestedFunction */',
+                false,
+            ],
+            'nested-closure' => [
+                '/* testNestedClosure */',
+                false,
+            ],
+            'class-method' => [
+                '/* testClassMethod */',
+                true,
+            ],
+            'class-nested-function' => [
+                '/* testClassNestedFunction */',
+                false,
+            ],
+            'class-nested-closure' => [
+                '/* testClassNestedClosure */',
+                false,
+            ],
+            'class-abstract-method' => [
+                '/* testClassAbstractMethod */',
+                true,
+            ],
+            'anon-class-method' => [
+                '/* testAnonClassMethod */',
+                true,
+            ],
+            'interface-method' => [
+                '/* testInterfaceMethod */',
+                true,
+            ],
+            'trait-method' => [
+                '/* testTraitMethod */',
+                true,
+            ],
+        ];
+    }
+}
