531c5b423140ab311e355ce8f08cedd66b512eac
jrfnl <jrfnl@users.noreply.github.com>
2020-02-04 20:57:37 +0100

    Documentation: update `@copyright` year

--- Tests/BackCompat/BCFile/FindExtendedClassNameTest.php
+++ Tests/BackCompat/BCFile/FindExtendedClassNameTest.php
@@ -3,7 +3,7 @@
  * PHPCSUtils, utility functions and classes for PHP_CodeSniffer sniff developers.
  *
  * @package   PHPCSUtils
- * @copyright 2019 PHPCSUtils Contributors
+ * @copyright 2019-2020 PHPCSUtils Contributors
  * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
  * @link      https://github.com/PHPCSStandards/PHPCSUtils
  *
