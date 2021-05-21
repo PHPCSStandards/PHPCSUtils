bd71b8ad3b821bc82d17175aa23bc4c0cdd6dd95
jrfnl <jrfnl@users.noreply.github.com>
2021-05-21 22:59:51 +0200

    Tests/IsShortArrayOrList: split test file [1]
    Split off the tests which are specific to the `IsShortArrayOrList::__construct()` method to a dedicated test file and add some additional tests to make sure the method is fully covered.

---
+++ Tests/Internal/IsShortArrayOrList/ConstructorTest.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * PHPCSUtils, utility functions and classes for PHP_CodeSniffer sniff developers.
+ *
+ * @package   PHPCSUtils
+ * @copyright 2019-2020 PHPCSUtils Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSUtils
+ */
+
+namespace PHPCSUtils\Tests\Internal\IsShortArrayOrList;
+
+use PHPCSUtils\Internal\IsShortArrayOrList;
+use PHPCSUtils\TestUtils\UtilityMethodTestCase;
+
+/**
+ * Tests for the \PHPCSUtils\Utils\IsShortArrayOrList class.
+ *
+ * @covers \PHPCSUtils\Internal\IsShortArrayOrList::__construct
+ *
+ * @since 1.0.0
+ */
+final class ConstructorTest extends UtilityMethodTestCase
+{
+
+    /**
+     * Test receiving an exception when passing a non-existent token pointer.
+     *
+     * @return void
+     */
+    public function testNonExistentToken()
+    {
+        $this->expectPhpcsException(
+            'The IsShortArrayOrList class expects to be passed a short array or square bracket token.'
+        );
+
+        new IsShortArrayOrList(self::$phpcsFile, 100000);
+    }
+
+    /**
+     * Test receiving an exception when a non-bracket token is passed.
+     *
+     * @dataProvider dataNotBracket
+     *
+     * @param string           $testMarker The comment which prefaces the target token in the test file.
+     * @param int|string|array $targetType The token type(s) to look for.
+     *
+     * @return void
+     */
+    public function testNotBracket($testMarker, $targetType)
+    {
+        $this->expectPhpcsException(
+            'The IsShortArrayOrList class expects to be passed a short array or square bracket token.'
+        );
+
+        $target = $this->getTargetToken($testMarker, $targetType);
+        new IsShortArrayOrList(self::$phpcsFile, $target);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNotBracket() For the array format.
+     *
+     * @return array
+     */
+    public function dataNotBracket()
+    {
+        return [
+            'long-array' => [
+                'testMarker' => '/* testLongArray */',
+                'targetType' => \T_ARRAY,
+            ],
+            'long-list' => [
+                'testMarker' => '/* testLongList */',
+                'targetType' => \T_LIST,
+            ],
+        ];
+    }
+
+    /**
+     * Test the constructor accepts all supported token types which are allowed to be passed to it.
+     *
+     * @dataProvider dataSupportedBrackets
+     *
+     * @param string           $testMarker The comment which prefaces the target token in the test file.
+     * @param int|string|array $targetType The token type(s) to look for.
+     *
+     * @return void
+     */
+    public function testSupportedBrackets($testMarker, $targetType)
+    {
+        $target = $this->getTargetToken($testMarker, $targetType);
+        $solver = new IsShortArrayOrList(self::$phpcsFile, $target);
+
+        $this->assertInstanceof('\PHPCSUtils\Internal\IsShortArrayOrList', $solver);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testSupportedBrackets() For the array format.
+     *
+     * @return array
+     */
+    public function dataSupportedBrackets()
+    {
+        return [
+            'short-array-open-bracket' => [
+                'testMarker' => '/* testShortArray */',
+                'targetType' => \T_OPEN_SHORT_ARRAY,
+            ],
+            'short-array-close-bracket' => [
+                'testMarker' => '/* testShortArray */',
+                'targetType' => \T_CLOSE_SHORT_ARRAY,
+            ],
+            'short-list-open-bracket' => [
+                'testMarker' => '/* testShortList */',
+                'targetType' => \T_OPEN_SHORT_ARRAY,
+            ],
+            'short-list-close-bracket' => [
+                'testMarker' => '/* testShortList */',
+                'targetType' => \T_CLOSE_SHORT_ARRAY,
+            ],
+            'square-brackets-open-bracket' => [
+                'testMarker' => '/* testSquareBrackets */',
+                'targetType' => \T_OPEN_SQUARE_BRACKET,
+            ],
+            'square-brackets-close-bracket' => [
+                'testMarker' => '/* testSquareBrackets */',
+                'targetType' => \T_CLOSE_SQUARE_BRACKET,
+            ],
+        ];
+    }
+}
