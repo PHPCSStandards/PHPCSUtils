39a6967f1dc2c0542f92b2be35590b4614e3ce7a
jrfnl <jrfnl@users.noreply.github.com>
2022-04-20 03:30:48 +0200

    BCFile::findStartOfStatement(): add tests for upstream bugfix for switch handling
    When inside a `switch` `case`/`default` `break`/`continue`/`return`/`exit`/`throw` statement, the start of the statement was not correctly determined.
    
    This adds unit tests safeguarding the upstream change.
    
    Refs:
    * squizlabs/php_codesniffer 3192
    * squizlabs/PHP_CodeSniffer 3186

--- Tests/BackCompat/BCFile/FindStartOfStatementTest.php
+++ Tests/BackCompat/BCFile/FindStartOfStatementTest.php
@@ -40,4 +40,110 @@ class FindStartOfStatementTest extends UtilityMethodTestCase
 
         $this->assertSame($expected, $found);
     }
+
+    /**
+     * Test finding the start of a statement inside a switch control structure case/default statement.
+     *
+     * @link https://github.com/squizlabs/php_codesniffer/issues/3192
+     * @link https://github.com/squizlabs/PHP_CodeSniffer/pull/3186/commits/18a0e54735bb9b3850fec266e5f4c50dacf618ea
+     *
+     * @dataProvider dataFindStartInsideSwitchCaseDefaultStatements
+     *
+     * @param string           $testMarker     The comment which prefaces the target token in the test file.
+     * @param array|string|int $targets        The token to search for after the test marker.
+     * @param string|int       $expectedTarget Token code of the expected start of statement stack pointer.
+     *
+     * @return void
+     */
+    public function testFindStartInsideSwitchCaseDefaultStatements($testMarker, $targets, $expectedTarget)
+    {
+        $testToken = $this->getTargetToken($testMarker, $targets);
+        $expected  = $this->getTargetToken($testMarker, $expectedTarget);
+
+        $found = BCFile::findStartOfStatement(self::$phpcsFile, $testToken);
+
+        $this->assertSame($expected, $found);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @return array
+     */
+    public static function dataFindStartInsideSwitchCaseDefaultStatements()
+    {
+        return [
+            'Case keyword should be start of case statement - case itself' => [
+                'testMarker'     => '/* testCaseStatement */',
+                'targets'        => \T_CASE,
+                'expectedTarget' => \T_CASE,
+            ],
+            'Case keyword should be start of case statement - number (what\'s being compared)' => [
+                'testMarker'     => '/* testCaseStatement */',
+                'targets'        => \T_LNUMBER,
+                'expectedTarget' => \T_CASE,
+            ],
+            'Variable should be start of arbitrary assignment statement - variable itself' => [
+                'testMarker'     => '/* testInsideCaseStatement */',
+                'targets'        => \T_VARIABLE,
+                'expectedTarget' => \T_VARIABLE,
+            ],
+            'Variable should be start of arbitrary assignment statement - equal sign' => [
+                'testMarker'     => '/* testInsideCaseStatement */',
+                'targets'        => \T_EQUAL,
+                'expectedTarget' => \T_VARIABLE,
+            ],
+            'Variable should be start of arbitrary assignment statement - function call' => [
+                'testMarker'     => '/* testInsideCaseStatement */',
+                'targets'        => \T_STRING,
+                'expectedTarget' => \T_VARIABLE,
+            ],
+            'Break should be start for contents of the break statement - contents' => [
+                'testMarker'     => '/* testInsideCaseBreakStatement */',
+                'targets'        => \T_LNUMBER,
+                'expectedTarget' => \T_BREAK,
+            ],
+            'Continue should be start for contents of the continue statement - contents' => [
+                'testMarker'     => '/* testInsideCaseContinueStatement */',
+                'targets'        => \T_LNUMBER,
+                'expectedTarget' => \T_CONTINUE,
+            ],
+            'Return should be start for contents of the return statement - contents' => [
+                'testMarker'     => '/* testInsideCaseReturnStatement */',
+                'targets'        => \T_FALSE,
+                'expectedTarget' => \T_RETURN,
+            ],
+            'Exit should be start for contents of the exit statement - close parenthesis' => [
+                // Note: not sure if this is actually correct - should this be the open parenthesis ?
+                'testMarker'     => '/* testInsideCaseExitStatement */',
+                'targets'        => \T_CLOSE_PARENTHESIS,
+                'expectedTarget' => \T_EXIT,
+            ],
+            'Throw should be start for contents of the throw statement - new keyword' => [
+                'testMarker'     => '/* testInsideCaseThrowStatement */',
+                'targets'        => \T_NEW,
+                'expectedTarget' => \T_THROW,
+            ],
+            'Throw should be start for contents of the throw statement - exception name' => [
+                'testMarker'     => '/* testInsideCaseThrowStatement */',
+                'targets'        => \T_STRING,
+                'expectedTarget' => \T_THROW,
+            ],
+            'Throw should be start for contents of the throw statement - close parenthesis' => [
+                'testMarker'     => '/* testInsideCaseThrowStatement */',
+                'targets'        => \T_CLOSE_PARENTHESIS,
+                'expectedTarget' => \T_THROW,
+            ],
+            'Default keyword should be start of default statement - default itself' => [
+                'testMarker'     => '/* testDefaultStatement */',
+                'targets'        => \T_DEFAULT,
+                'expectedTarget' => \T_DEFAULT,
+            ],
+            'Return should be start for contents of the return statement (inside default) - variable' => [
+                'testMarker'     => '/* testInsideDefaultContinueStatement */',
+                'targets'        => \T_VARIABLE,
+                'expectedTarget' => \T_CONTINUE,
+            ],
+        ];
+    }
 }
