b68597849a15b517f120564645c45011e7bfb0fa
jrfnl <jrfnl@users.noreply.github.com>
2022-10-05 16:36:10 +0200

    PHP 8.1 | FunctionDeclarations::getProperties(): add support for intersection types
    Ref:
    * squizlabs/PHP_CodeSniffer 3581
    
    Co-authored-by: Jaroslav Hanslík <kukulich@kukulich.cz>

--- PHPCSUtils/Tokens/Collections.php
+++ PHPCSUtils/Tokens/Collections.php
@@ -546,21 +546,23 @@ class Collections
      *
      * @since 1.0.0-alpha1
      * @since 1.0.0-alpha4 Added the T_TYPE_UNION, T_FALSE, T_NULL tokens for PHP 8.0 union type support.
+     * @since 1.0.0-alpha4 Added the T_TYPE_INTERSECTION token for PHP 8.1 intersection type support.
      *
      * @deprecated 1.0.0-alpha4 Use the {@see Collections::returnTypeTokens()} method instead.
      *
      * @var array <int|string> => <int|string>
      */
     public static $returnTypeTokens = [
-        \T_CALLABLE     => \T_CALLABLE,
-        \T_SELF         => \T_SELF,
-        \T_PARENT       => \T_PARENT,
-        \T_STATIC       => \T_STATIC,
-        \T_FALSE        => \T_FALSE,
-        \T_NULL         => \T_NULL,
-        \T_STRING       => \T_STRING,
-        \T_NS_SEPARATOR => \T_NS_SEPARATOR,
-        \T_TYPE_UNION   => \T_TYPE_UNION,
+        \T_CALLABLE          => \T_CALLABLE,
+        \T_SELF              => \T_SELF,
+        \T_PARENT            => \T_PARENT,
+        \T_STATIC            => \T_STATIC,
+        \T_FALSE             => \T_FALSE,
+        \T_NULL              => \T_NULL,
+        \T_STRING            => \T_STRING,
+        \T_NS_SEPARATOR      => \T_NS_SEPARATOR,
+        \T_TYPE_UNION        => \T_TYPE_UNION,
+        \T_TYPE_INTERSECTION => \T_TYPE_INTERSECTION,
     ];
 
     /**
@@ -991,6 +993,7 @@ class Collections
      * @since 1.0.0-alpha4 This method replaces the {@see Collections::$returnTypeTokens} property.
      * @since 1.0.0-alpha4 Added the T_TYPE_UNION, T_FALSE, T_NULL tokens for PHP 8.0 union type support.
      * @since 1.0.0-alpha4 Added support for PHP 8.0 identifier name tokens.
+     * @since 1.0.0-alpha4 Added the T_TYPE_INTERSECTION token for PHP 8.1 intersection type support.
      *
      * @return array <int|string> => <int|string>
      */
