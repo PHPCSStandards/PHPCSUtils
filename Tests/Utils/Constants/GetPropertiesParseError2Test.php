1223935c222ac3abeec37abeeeae1394b1dc7fa0
jrfnl <jrfnl@users.noreply.github.com>
2024-02-01 04:09:52 +0100

    :sparkles: New `PHPCSUtils\Utils\Constants` class
    ... to contain utilities methods for analysing constants declared using the `const` keyword.
    
    In the future, the scope of the methods might be expanded to also covered constants declared using `define()`, but that's for later.
    
    Initially, the class comes with the following method:
    * `getProperties(File $phpcsFile, $stackPtr): array` to retrieve an array of information about a constant declaration, like the visibility, whether visibility was explicitly declared, whether the constant was declared as final, what the type is for the constant etc.
    
    Includes extensive unit tests.

---
+++ Tests/Utils/Constants/GetPropertiesParseError2Test.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * PHPCSUtils, utility functions and classes for PHP_CodeSniffer sniff developers.
+ *
+ * @package   PHPCSUtils
+ * @copyright 2019-2024 PHPCSUtils Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSUtils
+ */
+
+namespace PHPCSUtils\Tests\Utils\Constants;
+
+use PHPCSUtils\TestUtils\UtilityMethodTestCase;
+use PHPCSUtils\Utils\Constants;
+
+/**
+ * Tests for the \PHPCSUtils\Utils\Constants::getProperties method.
+ *
+ * @covers \PHPCSUtils\Utils\Constants::getProperties
+ *
+ * @group constants
+ *
+ * @since 1.1.0
+ */
+final class GetPropertiesParseError2Test extends UtilityMethodTestCase
+{
+
+    /**
+     * Test receiving an exception when encountering a specific parse error.
+     *
+     * @return void
+     */
+    public function testParseError()
+    {
+        $this->expectPhpcsException('$stackPtr is not an OO constant');
+
+        $const = $this->getTargetToken('/* testParseErrorLiveCoding */', \T_CONST);
+        Constants::getProperties(self::$phpcsFile, $const);
+    }
+}
