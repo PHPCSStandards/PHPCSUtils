236fd0818abd0bce358cf62fab8bc9fbc58539ba
jrfnl <jrfnl@users.noreply.github.com>
2024-06-08 16:10:59 +0200

    TestUtils/SetUpTestFileTest: improve the test
    * Verify that the test class was virgin to begin with as otherwise the _real_ test would not be testing anything.
    * Verify for one more property that it's no longer at its default value.

--- Tests/TestUtils/UtilityMethodTestCase/SetUpTestFileTest.php
+++ Tests/TestUtils/UtilityMethodTestCase/SetUpTestFileTest.php
@@ -44,7 +44,20 @@ final class SetUpTestFileTest extends PolyfilledTestCase
      */
     public function testSetUp()
     {
+        // Verify that the class was virgin to begin with.
+        $this->assertSame('0', self::$phpcsVersion, 'phpcsVersion was not correct to begin with');
+        $this->assertSame('inc', self::$fileExtension, 'fileExtension was not correct to begin with');
+        $this->assertSame('', self::$caseFile, 'caseFile was not correct to begin with');
+        $this->assertSame(4, self::$tabWidth, 'tabWidth was not correct to begin with');
+        $this->assertNull(self::$phpcsFile, 'phpcsFile was not correct to begin with');
+        $this->assertSame(['Dummy.Dummy.Dummy'], self::$selectedSniff, 'selectedSniff was not correct to begin with');
+
+        // Run the set up.
         parent::setUpTestFile();
+
+        // Verify select properties have been set correctly.
+        $this->assertNotSame('0', self::$phpcsVersion, 'phpcsVersion was not set');
+
         $this->assertInstanceOf('PHP_CodeSniffer\Files\File', self::$phpcsFile);
         $this->assertSame(57, self::$phpcsFile->numTokens);
 
