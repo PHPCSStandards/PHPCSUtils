0644a1df27a50a8946b05050a9630b73d0cf74cf
jrfnl <jrfnl@users.noreply.github.com>
2024-10-07 05:35:21 +0200

    Tests/BCFile::find[Start|End]OfStatement(): sync in new tests from upstream [5]
    See PR PHPCSStandards/PHP_CodeSniffer 509 / https://github.com/PHPCSStandards/PHP_CodeSniffer/commit/a82f02e42fb5aad10bc6cdecbef00b9e07da5569

--- Tests/BackCompat/BCFile/FindStartOfStatementTest.php
+++ Tests/BackCompat/BCFile/FindStartOfStatementTest.php
@@ -10,6 +10,7 @@
 
 namespace PHPCSUtils\Tests\BackCompat\BCFile;
 
+use PHP_CodeSniffer\Util\Tokens;
 use PHPCSUtils\BackCompat\BCFile;
 use PHPCSUtils\TestUtils\UtilityMethodTestCase;
 
@@ -22,6 +23,40 @@ use PHPCSUtils\TestUtils\UtilityMethodTestCase;
  */
 final class FindStartOfStatementTest extends UtilityMethodTestCase
 {
+
+    /**
+     * Test that start of statement is NEVER beyond the "current" token.
+     *
+     * @return void
+     */
+    public function testStartIsNeverMoreThanCurrentToken()
+    {
+        $tokens = self::$phpcsFile->getTokens();
+        $errors = [];
+
+        for ($i = 0; $i < self::$phpcsFile->numTokens; $i++) {
+            if (isset(Tokens::$emptyTokens[$tokens[$i]['code']]) === true) {
+                continue;
+            }
+
+            $start = BCFile::findStartOfStatement(self::$phpcsFile, $i);
+
+            // Collect all the errors.
+            if ($start > $i) {
+                $errors[] = sprintf(
+                    'Start of statement for token %1$d (%2$s: %3$s) on line %4$d is %5$d (%6$s), which is more than %1$d',
+                    $i,
+                    $tokens[$i]['type'],
+                    $tokens[$i]['content'],
+                    $tokens[$i]['line'],
+                    $start,
+                    $tokens[$start]['type']
+                );
+            }
+        }
+
+        $this->assertSame([], $errors);
+    }
 
     /**
      * Test a simple assignment.
