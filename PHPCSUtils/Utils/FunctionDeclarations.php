f34f2d451837735cdccc2017fdfb79ac91503dca
jrfnl <jrfnl@users.noreply.github.com>
2025-06-03 16:59:24 +0200

    FunctionDeclarations::getProperties(): minor simplification / T_USE is parenthesis owner
    Minor simplification as, as of PHPCS 4.0, `T_USE` is a parentheses owner and we no longer need to work out the position of the parenthesis closer for PHPCS 3.x.
    
    Ref: PHPCSStandards/PHP_CodeSniffer 1013

--- PHPCSUtils/Utils/FunctionDeclarations.php
+++ PHPCSUtils/Utils/FunctionDeclarations.php
@@ -281,23 +281,15 @@ final class FunctionDeclarations
                     break;
                 }
 
+                // Skip over closure use statements.
                 if ($tokens[$i]['code'] === \T_USE) {
-                    // Skip over closure use statements.
-                    for (
-                        $j = ($i + 1);
-                        $j < $phpcsFile->numTokens && isset(Tokens::$emptyTokens[$tokens[$j]['code']]) === true;
-                        $j++
-                    );
-
-                    if ($tokens[$j]['code'] === \T_OPEN_PARENTHESIS) {
-                        if (isset($tokens[$j]['parenthesis_closer']) === false) {
-                            // Live coding/parse error, stop parsing.
-                            break;
-                        }
-
-                        $i = $tokens[$j]['parenthesis_closer'];
-                        continue;
+                    if (isset($tokens[$i]['parenthesis_closer']) === false) {
+                        // Live coding/parse error, stop parsing.
+                        break;
                     }
+
+                    $i = $tokens[$i]['parenthesis_closer'];
+                    continue;
                 }
 
                 if ($tokens[$i]['code'] === \T_NULLABLE) {
