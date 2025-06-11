a4ff3f6955559f4108041d8b2f17845c42508ed8
jrfnl <jrfnl@users.noreply.github.com>
2025-06-11 10:20:11 +0200

    FileInfo::hasByteOrderMark(): add some extra tests
    Includes fixing the grammar of some test documentation.

---
+++ Tests/Utils/FileInfo/HasByteOrderMarkAnsiWithInlineHTMLTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * PHPCSUtils, utility functions and classes for PHP_CodeSniffer sniff developers.
+ *
+ * @package   PHPCSUtils
+ * @copyright 2019-2024 PHPCSUtils Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSUtils
+ */
+
+namespace PHPCSUtils\Tests\Utils\FileInfo;
+
+use PHPCSUtils\TestUtils\UtilityMethodTestCase;
+use PHPCSUtils\Utils\FileInfo;
+
+/**
+ * Tests for the \PHPCSUtils\Utils\FileInfo::hasByteOrderMark() method.
+ *
+ * @covers \PHPCSUtils\Utils\FileInfo::hasByteOrderMark
+ *
+ * @since 1.1.0
+ */
+final class HasByteOrderMarkAnsiWithInlineHTMLTest extends UtilityMethodTestCase
+{
+
+    /**
+     * Test whether a file without byte order mark at the start of the file is correctly recognized.
+     *
+     * @return void
+     */
+    public function testHasByteOrderMark()
+    {
+        $this->assertFalse(FileInfo::hasByteOrderMark(self::$phpcsFile));
+    }
+}
