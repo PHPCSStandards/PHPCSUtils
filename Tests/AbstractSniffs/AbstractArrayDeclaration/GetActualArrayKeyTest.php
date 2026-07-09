e8648633ff5b8219b1b3e6346ce85abfc30f1429
jrfnl <jrfnl@users.noreply.github.com>
2026-07-09 17:07:18 +0200

    AbstractArrayDeclarationSniff::getActualArrayKey(): prevent executing arbitrary callback
    This is a security fix for CVE-2026-65954 / [GHSA-r6hr-vr92-vv28](https://github.com/PHPCSStandards/PHPCSUtils/security/advisories/GHSA-r6hr-vr92-vv28).
    
    As things were, it was possible for an arbitrary function call using callback format to be executed via `eval()` when the `AbstractArrayDeclarationSniff::getActualArrayKey()` method was used by a sniff.
    
    This would require for the "code under scan" to have a function call using callback format in the key of an array item.
    Worst case scenario, this function call could result in system commands being run on the local system running the PHPCS scan.
    
    Fixed now by bowing out for any syntax combination which _could_ indicate that the code in question is part of a function call using callback format.
    
    Includes tests.
    
    Note: the included tests have been specially crafted to be safe, even if they would be failing.

--- Tests/AbstractSniffs/AbstractArrayDeclaration/GetActualArrayKeyTest.php
+++ Tests/AbstractSniffs/AbstractArrayDeclaration/GetActualArrayKeyTest.php
@@ -220,4 +220,51 @@ final class GetActualArrayKeyTest extends UtilityMethodTestCase
         $actualKeys   = \array_keys(\array_combine($expected, $expected));
         $this->assertSame($expectedKeys, $actualKeys, 'getActualArrayKey() results do not match PHP native handling');
     }
+
+    /**
+     * Verify that callbacks in array keys are not executed.
+     *
+     * @dataProvider dataGetActualArrayKeyDoesNotExecuteCallbacks
+     *
+     * @param string $testMarker The comment which prefaces the target token in the test file.
+     *
+     * @return void
+     */
+    public function testGetActualArrayKeyDoesNotExecuteCallbacks($testMarker)
+    {
+        $testObj         = new ArrayDeclarationSniffTestDouble();
+        $testObj->tokens = self::$phpcsFile->getTokens();
+
+        $stackPtr   = $this->getTargetToken($testMarker, [\T_ARRAY, \T_OPEN_SHORT_ARRAY]);
+        $arrayItems = PassedParameters::getParameters(self::$phpcsFile, $stackPtr);
+
+        foreach ($arrayItems as $itemNr => $arrayItem) {
+            $arrowPtr = Arrays::getDoubleArrowPtr(self::$phpcsFile, $arrayItem['start'], $arrayItem['end']);
+            if ($arrowPtr !== false) {
+                $result = $testObj->getActualArrayKey(self::$phpcsFile, $arrayItem['start'], ($arrowPtr - 1));
+                $this->assertNull(
+                    $result,
+                    'Failed: actual key ' . \var_export($result, true) . ' is not void for item number ' . $itemNr
+                );
+            }
+        }
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testGetActualArrayKeyDoesNotExecuteCallbacks() For the array format.
+     *
+     * @return array<string, array<string>>
+     */
+    public static function dataGetActualArrayKeyDoesNotExecuteCallbacks()
+    {
+        $data = [];
+
+        for ($i = 1; $i <= 9; $i++) {
+            $data['callback-type-' . $i] = ['/* testCallbackInKeyResultsInVoid' . $i . ' */'];
+        }
+
+        return $data;
+    }
 }
